6c49dabe319fc8c7a44eede4257ebba6931fd1a1
Ken Mazaika <kenmazaika@gmail.com>
2013-05-13 14:45:19 -0400

    If we're already in the process of Marshaling the file to the cache do not re-marshal it, which can cause issues when done in concurrent threads in race conditions.

--- lib/sprockets/cache/file_store.rb
+++ lib/sprockets/cache/file_store.rb
@@ -16,15 +16,33 @@ module Sprockets
       # Lookup value in cache
       def [](key)
         pathname = @root.join(key)
-        pathname.exist? ? pathname.open('rb') { |f| Marshal.load(f) } : nil
+
+        if pathname.exist?  
+          pathname.open('rb') { |f| Marshal.load(f) }
+        else
+          nil
+        end
       end
 
       # Save value to cache
       def []=(key, value)
         # Ensure directory exists
-        FileUtils.mkdir_p @root.join(key).dirname
+        FileUtils.mkdir_p @root.join("#{key}.lock").dirname
 
-        @root.join(key).open('w') { |f| Marshal.dump(value, f)}
+        if @root.join("#{key}.lock").exist?
+          puts "File Locked; Currently caching to this file, so skipping"
+        else
+          # Lock the File to prevent threads from stomping on each other.
+          @root.join("#{key}.lock").open('wb') {|f| f.puts 'locked' }
+
+          # Atomic write
+          @root.join("#{key}+").open('wb') { |f| Marshal.dump(value, f)}
+          FileUtils.mv(@root.join("#{key}+"), @root.join(key))
+
+          # Unlock
+          FileUtils.rm_rf @root.join("#{key}.lock")
+        end
+
         value
       end
     end
